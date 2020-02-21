2c248910376bed176f406cd8a8b06040b4017a54
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-02-21 12:17:14 -0500

    Updates

---
+++ precache-manifest.d7e23ea038a5714eee4be31051df265f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a6402286225d99fc081d98bc94aeea",
+    "url": "/control-patio/index.html"
+  },
+  {
+    "revision": "0b0387ea6d4c1d419142",
+    "url": "/control-patio/static/css/main.1f4d1037.chunk.css"
+  },
+  {
+    "revision": "a1b3f21c54129b15af7e",
+    "url": "/control-patio/static/js/2.2971381f.chunk.js"
+  },
+  {
+    "revision": "26e91145a1815a65a03e0328ce0b5201",
+    "url": "/control-patio/static/js/2.2971381f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b0387ea6d4c1d419142",
+    "url": "/control-patio/static/js/main.62700fff.chunk.js"
+  },
+  {
+    "revision": "7b4207b11436190b8f06",
+    "url": "/control-patio/static/js/runtime-main.fb1537fa.js"
+  }
+]);
